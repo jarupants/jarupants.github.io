d9d791e322587af2daaa0d4edaad6d775e008bd9
quingulus <dangquiana@gmail.com>
2025-04-24 16:59:03 -0400

    adding work, more button logic changes, background colors

--- javascript.js
+++ javascript.js
@@ -1,5 +1,29 @@
 // javascript.js
 
+// About active toggle
+
+var aboutButton = document.querySelector("#aboutButton");
+var aboutList = document.querySelectorAll("#about");
+
+var resumeButton = document.querySelector("#resumeButton");
+var workButton = document.querySelector("#workButton");
+var homeButton = document.querySelector("#homeButton");
+
+var activePage;
+var sidebarContainer = document.querySelector("#sidebar-container");
+
+var resumeSidebar = document.querySelector("#resume");
+var workSidebar = document.querySelector("#work");
+
+var title1 = document.querySelector("#titleline1");
+var title2 = document.querySelector("#titleline2");
+var title3 = document.querySelector("#titleline3");
+
+let isAboutActive = false;
+let isSidebarActive = false;
+let isHomeActive = false;
+let isHomeActivatable = false;
+
 // Gradient change
 
 function changeGradient(category) {
@@ -8,6 +32,10 @@ function changeGradient(category) {
         case "home":
             bgGradient.style.background = "radial-gradient(circle at center 80%, rgba(242,40,40,1.0) 0%, rgba(242,40,40,1.0) 17%, rgba(255,87,76,1.0) 35%, rgba(255,105,105,1.0) 44%, rgba(255,248,241,1.0) 75%) no-repeat";
             console.log("home");
+            break;
+        case "work":
+            bgGradient.style.background = "radial-gradient(circle at center 80%, rgba(250,65,145,1.0) 0%, rgba(254,79,155,1.0) 17%, rgba(255,115,159,1.0) 35%, rgba(255,137,167,1.0) 44%, rgba(255,248,241,1.0) 75%) no-repeat";
+            console.log("about");
             break;
         case "resume":
             bgGradient.style.background = "radial-gradient(circle at center 80%, rgba(56,40,242,1.0) 0%, rgba(129,40,242,1.0) 17%, rgba(176,76,255,1.0) 35%, rgba(198,105,255,1.0) 44%, rgba(255,248,241,1.0) 75%) no-repeat";
@@ -24,21 +52,6 @@ function changeGradient(category) {
     }
 }
 
-// About active toggle
-
-var aboutButton = document.querySelector("#aboutButton");
-var aboutList = document.querySelectorAll(".about-sidebar");
-var menuButton = document.querySelector("#resumeButton");
-var homeButton = document.querySelector("#homeButton");
-
-var sidebar = document.querySelector("#resume-sidebar");
-var title1 = document.querySelector("#titleline1");
-var title2 = document.querySelector("#titleline2");
-var title3 = document.querySelector("#titleline3");
-let isAboutActive = false;
-let isSidebarActive = false;
-let isHomeActive = false;
-let isHomeActivatable = false;
 
 function setHomeButtonOnLoad() {
     homeButton.className = "on";
@@ -47,17 +60,20 @@ function setHomeButtonOnLoad() {
 document.addEventListener("DOMContentLoaded", setHomeButtonOnLoad);
 
 function setActive(Object) {
+
     isHomeActive = false;
     Object.toggleAttribute("active");
     Object.style.display = "flex";
 }
 
 function setInactive(Object) {
+
     Object.toggleAttribute("active");
     Object.style.display = "none";
 }
 
 function setAboutHeight() {
+
     if (isAboutActive == true) {
         aboutList.forEach(setActive);
         title1.style.display = "none";
@@ -66,6 +82,7 @@ function setAboutHeight() {
         changeGradient("about");
         aboutButton.className = "on";
         homeButton.className = "menu";
+
     } else if (isAboutActive == false) {
         aboutList.forEach(setInactive);
         title1.style.display = "inline";
@@ -76,6 +93,7 @@ function setAboutHeight() {
 }
 
 function setAboutState() {
+
         isAboutActive = !isAboutActive;
         setAboutHeight();
     
@@ -94,21 +112,51 @@ aboutButton.addEventListener("click", setAboutState);
 // Sidebar active toggle
 
 function setSidebarWidth() {
+
     if (isSidebarActive == true) {
-        sidebar.style.maxWidth = "400px";
-        setActive(sidebar);
-        changeGradient("resume");
-        menuButton.className = "on";
+
+        switch(activePage) {
+
+            case "resume":
+                changeGradient("resume");
+                resumeSidebar.style.width = "30dvw";
+                resumeButton.className = "on";
+                setActive(resumeSidebar);
+
+                setInactive(workSidebar);
+                workButton.className = "menu";
+
+                setActive(sidebarContainer);
+                break;
+            case "work":
+                changeGradient("work");
+                workSidebar.style.width = "420px";
+                workButton.className = "on";
+                setActive(workSidebar);
+
+                setInactive(resumeSidebar);
+                resumeButton.className = "menu";
+
+                setActive(sidebarContainer);
+                break;
+            default:
+                break;
+        };
+
         homeButton.className = "menu";
+
     } else if (isSidebarActive == false) {
-        sidebar.style.maxWidth = "0px";
-        setInactive(sidebar);
-        menuButton.className = "menu";
+        
+        setInactive(resumeSidebar);
+        setInactive(workSidebar);
+        setInactive(sidebarContainer);
+        resumeButton.className = "menu";
+        workButton.className = "menu";
     }
 }
 
 function setSidebarState() {
-    isSidebarActive = !isSidebarActive;
+
     setSidebarWidth();
 
     isAboutActive = false;
@@ -121,8 +169,24 @@ function setSidebarState() {
     isHomeActivatable = !isHomeActive;
 }
 
-menuButton.addEventListener("click", setSidebarState);
+function setActivePage(page) {
+    activePage = page;
+    console.log(activePage);
+}
 
+resumeButton.addEventListener("click", function() {
+    setActivePage("resume");
+    isSidebarActive = true;
+    setSidebarState();
+    console.log("resume");
+    });
+
+workButton.addEventListener("click", function() {
+    setActivePage("work");
+    isSidebarActive = true;
+    setSidebarState();
+    console.log("work");
+    });  
 
 // Home reset
 
@@ -132,6 +196,7 @@ function setHomeState() {
 
     changeGradient("home");
     homeButton.className = "on";
+    setActivePage("home");
 
     isAboutActive = false;
     isSidebarActive = false;
@@ -147,6 +212,7 @@ function setHomeState() {
 
         changeGradient("home");
         homeButton.className = "on";
+        setActivePage("home");
 
         isAboutActive = false;
         isSidebarActive = false;
--- project_component.js
+++ project_component.js
@@ -1,19 +1,24 @@
+let project = document.querySelector("project-item");
+
 const projectArray = [
-    { title: 'Project', role: 'UX' },
-    { title: 'Project2', role: 'UX2' },
+    { title: '2XKO: Progression', role: 'UX', img: './images/logo_2xko.png' },
+    { title: '2XKO: <br>Player Profile', role: 'Full Stack', img: './images/logo_2xko.png' },
+    { title: '2XKO: Postmatch', role: 'UX', img: './images/logo_2xko.png' },
+    { title: '2XKO: <br>Lobby', role: 'UI', img: './images/logo_2xko.png' },
+    { title: 'Pool Party: Music Player', role: 'UX', img: './images/logo_riot.jpg' },
+    { title: 'Fortnite Festival', role: 'UX | UI', img: './images/logo_fn.jpg' },
 ]
 
-class Project extends HTMLElement {
-    connectedCallback() {
-            this.innerHTML = `
+projectArray.forEach(value => {
+    const currentElement = document.createElement("a");
+    currentElement.innerHTML = `
         <div class="project">
-            <img class="project"src="./images/meee.png" width="100" height="100">
-            <h3 class="project">I'm title</h3>
-            <p id="project-title">I'm role</p>
+            <img class="project" src=${value.img} width="100" height="100">
+            <h3 class="project">${value.title}</h3>
+            <hr style="width:100%;height:1px;border-top:#009A7E;color:#009A7E;background-color:#009A7E;border-left:#009A7E;border-right:#009A7E;"></hr>
+            <p id="project-role">${value.role}</p>
         </div> 
-        `;
-        }
-    }
-
-customElements.define('project-item', Project);
-    
+    `;
+    project.before(currentElement);
+})
+       
